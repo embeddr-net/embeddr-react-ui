359df0b7af5fb4c9186c25a60da1a623739855de
Nynxz <contact@nynxz.com>
2026-05-01 07:31:33 +1000

    fix: inline prettier config (was referencing parent dir)
    The prior `require("../../prettier.config.cjs")` only worked when this
    repo was cloned inside a sibling-of-sibling layout. Since this is a
    standalone published package, all configs must be self-contained.

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,1 +1,8 @@
-module.exports = require("../../prettier.config.cjs");
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 100,
+  tabWidth: 2,
+};
